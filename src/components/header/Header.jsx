4153f77c31790252c364a2255dceac91097aa19c
Guilherme Rocha <guirocha1938@gmail.com>
2022-04-01 16:58:40 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
+import styles from './Header.module.css'
+
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
-import styles from './Header.module.css'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Guilherme Rocha</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" size="50px" link="https://github.com/SStyum" />
+      </div>
     </div>
   )
 }
