67f98288206c655fc900dfdc03ef342b57f6e7ba
Guilherme Rocha <guirocha1938@gmail.com>
2022-04-01 16:41:46 -0300

    Inicializando projeto

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import GamePage from '../src/components/gamePage/GamePage'
+function HomePage () {
+  
+  return (
+    <div>
+      <GamePage />
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import Title from '../title/Title'
+import Subtitle from '../subtitle/Subtitle'
+import styles from './Header.module.css'
+
+function Header () {
+  return (
+    <div className={styles.header}>
+      <Title>Jogo da Velha</Title>
+      <Subtitle>Criado por Guilherme Rocha</Subtitle>
+    </div>
+  )
+}
+
+export default Header
