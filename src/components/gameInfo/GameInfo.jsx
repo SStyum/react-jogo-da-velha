ab26abb90e29748e32fdf2d0b9406dc3fbca5eb0
Guilherme Rocha <guirocha1938@gmail.com>
2022-04-06 14:02:56 -0300

    Criada mecânica de empate

--- src/components/gameInfo/GameInfo.jsx
+++ src/components/gameInfo/GameInfo.jsx
@@ -2,15 +2,16 @@ import Icon from '../icon/Icon'
 import styles from './GameInfo.module.css'
 import Button from '../button/Button'
 
-function GameInfo ({currentPlayer, winner, onReset}) {
+function GameInfo ({currentPlayer, winner, onReset, isDraw}) {
   
   const shouldEnableButton = () => {
     if(winner !== 0) return true
+    if(isDraw) return true
   }
   return (
     <div className={styles.gameInfo}>
       {
-        winner === 0 &&
+        !isDraw && winner === 0 &&
           <>
 
             <h4>Próximo a jogar: </h4>
@@ -23,7 +24,7 @@ function GameInfo ({currentPlayer, winner, onReset}) {
           </>
       }
       {
-        winner !== 0 &&
+        !isDraw && winner !== 0 &&
         <>
           <h4>Fim de jogo! Campeão:</h4>
           {
@@ -33,6 +34,9 @@ function GameInfo ({currentPlayer, winner, onReset}) {
             winner === -1 && <Icon iconName="x" />
           }
         </>
+      }
+      {
+        isDraw && <h4>Empate!</h4>
       }
       <Button
         onClick={onReset}
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -3,10 +3,11 @@ import Icon from '../icon/Icon'
 import classNames from 'classnames'
 
 const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px"/>
-function GameOption ({status, onClick, isWinner}) {
+function GameOption ({status, onClick, isWinner, isDraw}) {
   return (
     <div className={classNames(styles.gameOption, {
-      [styles.winner]: isWinner
+      [styles.winner]: isWinner,
+      [styles.draw]: isDraw
     })} onClick={onClick}>
       {
         status === 1 && <GameIcon iconName="circle" />
